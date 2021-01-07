2d029e87a1db9fbf2b80478d5ef7843a9de7f758
luis garcia <01800roc@gmail.com>
2021-01-07 17:16:04 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+(function inicio() {
+    const home = document.getElementById("icon_rocket").addEventListener("click",pagini);
+
+})();
+
+function pagini() {
+    window.location="file:///C:/Users/roc%20eagle/Desktop/tareas/index.html";
+}
+
+
